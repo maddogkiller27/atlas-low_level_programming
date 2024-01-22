c58d79901bc029e524d7abc10681dca4809ce2d2
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-22 21:32:02 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints base 16 #'s in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numbr;
+char letter;
+
+for (numbr = 0; numbr < 10; numbr++)
+putchar((num % 10) + '0');
+
+for (letter = 'a'; letter <= 'f'; letter++)
+
+putchar('\n');
+
+return (0);
+}
