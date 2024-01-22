d3b7997343014c44c5da9b09e1c78e7d40b7b228
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-22 21:08:48 +0000

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numbr;
+
+for (numbr = 0; numbr < 10; numbr++)
+printf("%d", numbr);
+
+printf("\n");
+
+return (0);
+}
