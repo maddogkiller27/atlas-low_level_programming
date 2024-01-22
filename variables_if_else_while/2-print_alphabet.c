9d68d1c65cdc4f190f990946682addedaf777c3c
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-22 20:27:15 +0000

    2-print_alph

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <='z'; letter++)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
