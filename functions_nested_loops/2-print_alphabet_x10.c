1846c13ed00e630b485ad1fd63cac7b4294d2890
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-23 00:17:39 +0000

    mnrERR-FIX

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,8 @@ int count = 0;
 char letter;
 
 while (count++ <= 9)
-{for (letter = 'a'; letter <= 'z'; letter++)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar ('\n');
 }
