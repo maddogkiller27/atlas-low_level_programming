b70c8fcaea5f7a4eb28f8c05e1dd7b4fb123dd08
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-24 23:30:59 +0000

    im_trying

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase letter
+ * @c: letter being checked
+ *
+ * Return: 1 if upper 0 if lower
+ */
+
+int _isupper(int c);
+{
+  if (c
