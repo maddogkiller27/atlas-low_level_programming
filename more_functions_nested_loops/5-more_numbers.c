c6a5c8b309ce5954b8ef59ed4f209e2ca158459d
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-26 04:47:35 +0000

    5-first_try

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0-14 ten times
+ *
+ * Return: idk, im not feeling so smart anymore
+ */
+
+void more_numbers(void)
+{
+int loop = '1';
+int number;
+
+while (loop <=10)
+{
+for (number = '0'; number <= (14); number++)
+_putchar(number);
+}
+int loop = ++;
+_putchar('\n');
+}
