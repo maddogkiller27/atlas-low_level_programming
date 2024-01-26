495f8cffd1d7bd5f809a07ab150a426fcfe99ca2
maddogkiller27 <maddogkiller27@gmail.com>
2024-01-26 04:19:27 +0000

    4-first_try

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+int number;
+
+for (number = '0'; number <= '9'; number++)
+{
+if (number == '2'|| number == '4'; number++);
+}
+_putchar(number);
+_putchar('\n');
+}
