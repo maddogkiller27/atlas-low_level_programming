4f54f01216bec3d4c3a5881b80dadd66c5d1d58f
maddogkiller27 <maddogiller27@gmail.com>
2024-01-27 03:19:56 +0000

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of square
+ *
+ * Return: 0
+ */
+
+void print_square(int size);
+{
+int height;
+int width;
+
+if (size > 0)
+{
+for (height = 0; height < size; height++)
+{
+for (width = 0; width < size; width++)
+_putchar('#');
+if (height == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar)'\n');
+}
